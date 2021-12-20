39e8787a9de6c251789847a5e01395692b9cfdc8
dtsikaridze <dtsikaridze@tbcbank.com.ge>
2021-12-20 22:25:01 +0400

    add home component and update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChampionsComponent } from './champions/champions.component';
 import { DocumentationComponent } from './documentation/documentation.component';
+import { HomeComponent } from './home/home.component';
 import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'find-summoner', component: MainComponent },
   { path: 'documentation', component: DocumentationComponent },
   { path: 'champions', component: ChampionsComponent },
