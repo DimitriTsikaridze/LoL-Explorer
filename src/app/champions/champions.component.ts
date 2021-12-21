446a41be21f26d444f4efac31c577bc067911b68
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2021-12-21 17:10:23 +0400

    update champions service

--- src/app/champions/champions.component.ts
+++ src/app/champions/champions.component.ts
@@ -11,11 +11,13 @@ export class ChampionsComponent implements OnInit {
   constructor(private championsService: ChampionsService) {}
   champions!: Champion[];
 
-  ngOnInit(): void {}
-
-  onGetChampions() {
-    this.championsService.getChampions('splash').subscribe((championsData) => {
-      this.champions = championsData.slice(0, 3);
-    });
+  ngOnInit(): void {
+    this.championsService
+      .getChampions('tiles', 5)
+      .subscribe((championsData) => {
+        this.champions = championsData;
+      });
   }
+
+  onGetChampions() {}
 }
