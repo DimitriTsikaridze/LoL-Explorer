543e87907a4029e93a22265b0b076c370eb732f4
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2021-12-28 13:32:15 +0400

    refactor: remove angular material and shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
+import { HeaderComponent } from './header/header.component';
+import { DocumentationComponent } from './documentation/documentation.component';
+import { HomeComponent } from './home/home.component';
+import { SearchSummonerComponent } from './search-summoner/search-summoner.component';
+import { ChampionsComponent } from './champions/champions.component';
+import { SingleChampionComponent } from './champions/single-champion/single-champion.component';
+import { ChampionDetailsComponent } from './champions/champion-details/champion-details.component';
+
+const components = [
+  HeaderComponent,
+  DocumentationComponent,
+  SearchSummonerComponent,
+  HomeComponent,
+  ChampionsComponent,
+  SingleChampionComponent,
+  ChampionDetailsComponent,
+];
+
+const modules = [
+  HttpClientModule,
+  ReactiveFormsModule,
+  RouterModule,
+  AppRoutingModule,
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, SharedModule],
+  declarations: [AppComponent, ...components],
+  imports: [BrowserModule, BrowserAnimationsModule, ...modules],
   providers: [],
   bootstrap: [AppComponent],
 })
