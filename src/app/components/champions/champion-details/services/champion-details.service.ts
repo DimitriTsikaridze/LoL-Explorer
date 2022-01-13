685919fae65a15bcc91e843329931e713465f4db
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2022-01-13 11:35:13 +0400

    stlye: add champion details

--- src/app/components/champions/champion-details/services/champion-details.service.ts
+++ src/app/components/champions/champion-details/services/champion-details.service.ts
@@ -24,7 +24,7 @@ export class ChampionDetailsService {
       .get<ChampionDetails>(`${this.championDetailsURL}${championID}.json`)
       .pipe(
         map((value: any) => {
-          const { id, key, lore, name, title, allytips, enemytips } =
+          const { id, key, lore, name, title, allytips, enemytips, tags } =
             value.data[championID];
           this.champion = {
             id: id,
@@ -34,6 +34,7 @@ export class ChampionDetailsService {
             title: title,
             allyTips: allytips,
             enemyTips: enemytips,
+            tags: tags,
             imageURL: `${this.championsService.championsURL}splash/${championID}_0.jpg`,
           };
           return this.champion;
