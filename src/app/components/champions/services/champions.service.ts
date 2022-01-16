28b21c930301507250ba601462853e0c8ccfa768
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2022-01-16 23:55:18 +0400

    refactor: replace multiple service URLs with shared service

--- src/app/components/champions/services/champions.service.ts
+++ src/app/components/champions/services/champions.service.ts
@@ -1,24 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs';
+import { RiotApiService } from '../../../riot-api.service';
 import { Champion, ChampionResponse } from '../models/champion-response';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChampionsService {
-  readonly URL =
-    'https://ddragon.leagueoflegends.com/cdn/11.24.1/data/en_US/champion.json';
-
-  readonly championsURL =
-    'https://ddragon.leagueoflegends.com/cdn/img/champion/';
-
   readonly champions: Champion[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private riotAPI: RiotApiService) {}
 
-  getChampions(imageType: 'splash' | 'loading' | 'tiles' | 'centered') {
-    return this.http.get<ChampionResponse>(this.URL).pipe(
+  getChampions() {
+    return this.http.get<ChampionResponse>(this.riotAPI.championsURL).pipe(
       map((value: ChampionResponse) => {
         for (let champion in value.data) {
           const { id, key, name, blurb, title, tags } = value.data[champion];
@@ -29,7 +24,7 @@ export class ChampionsService {
             blurb: blurb,
             title: title,
             tags: tags,
-            imageURL: `${this.championsURL}${imageType}/${value.data[champion].id}_0.jpg`,
+            imageURL: `${this.riotAPI.championIconURL}splash/${value.data[champion].id}_0.jpg`,
           });
         }
         return this.champions;
