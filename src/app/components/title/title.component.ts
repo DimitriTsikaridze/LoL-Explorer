826addb01132f6dc0633e6a240f34c4b6af4d9f0
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2022-01-17 19:28:55 +0400

    fix: title underline overflow

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -2,7 +2,11 @@ import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
-  template: ` <h1>{{ title }}</h1> `,
+  template: `
+    <div>
+      <h1>{{ title }}</h1>
+    </div>
+  `,
   styleUrls: ['./title.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
