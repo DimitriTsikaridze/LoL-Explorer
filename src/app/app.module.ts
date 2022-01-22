a04442279257d002cfe0f0432c1d9a58d493bd71
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2022-01-22 20:54:58 +0400

    add search bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -30,7 +29,6 @@ const components = [
 
 const modules = [
   HttpClientModule,
-  ReactiveFormsModule,
   RouterModule,
   AppRoutingModule,
   SharedModule,
