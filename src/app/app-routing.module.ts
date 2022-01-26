8529b585b8c9569d9a863172c3665f1799ab5365
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2022-01-26 21:42:58 +0400

    remove unused component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChampionDetailsComponent } from './components/champions/champion-details/champion-details.component';
 import { ChampionsComponent } from './components/champions/champions.component';
-import { DocumentationComponent } from './components/documentation/documentation.component';
 import { ErrorComponent } from './components/error/error.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchSummonerComponent } from './components/search-summoner/search-summoner.component';
@@ -11,7 +10,6 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'search-summoner', component: SearchSummonerComponent },
-  { path: 'documentation', component: DocumentationComponent },
   { path: 'champions', component: ChampionsComponent },
   { path: 'champions/:id', component: ChampionDetailsComponent },
   { path: 'not-found', component: ErrorComponent },
