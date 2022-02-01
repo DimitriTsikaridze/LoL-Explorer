86c00373a234ff0ea952790036272f3a32f99747
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2022-02-01 22:24:06 +0400

    feat(skins): add skins

---
+++ src/app/components/champions/champion-details/skins/skin-service.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SkinServiceService {
+  private SKINS_URL =
+    'https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champions/';
+
+  skins: string[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  getSkins(id: string) {
+    return this.http.get(`${this.SKINS_URL}${id}.json`).pipe(
+      map((data: any) => {
+        data.skins.forEach((skin: any) => {
+          this.skins.push(skin.splashPath.slice(skin.splashPath.indexOf('v1')));
+        });
+        return this.skins;
+      })
+    );
+  }
+}
