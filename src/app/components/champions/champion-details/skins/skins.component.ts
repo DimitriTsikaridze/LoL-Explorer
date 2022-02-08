f9d753cba715c05483be900f59efe323452dfed5
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2022-02-08 23:31:06 +0400

    add redirect url on skin click

--- src/app/components/champions/champion-details/skins/skins.component.ts
+++ src/app/components/champions/champion-details/skins/skins.component.ts
@@ -11,10 +11,15 @@ import { SkinServiceService } from './skin-service.service';
 export class SkinsComponent implements OnInit {
   @Input() champion!: ChampionDetails;
   skins!: Observable<string[]>;
+  skinURL: string = '';
 
   constructor(private skinService: SkinServiceService) {}
 
   ngOnInit(): void {
     this.skins = this.skinService.getSkins(this.champion.key);
+  }
+
+  onNavigateToSkin(id: any) {
+    this.skinURL = `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/${id}`;
   }
 }
