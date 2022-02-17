c89833dd9d4b3d4087e77c1921bd5d266241db39
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2022-02-17 23:28:29 +0400

    refactor: add renderer2

--- src/app/directives/zoom-in.directive.ts
+++ src/app/directives/zoom-in.directive.ts
@@ -1,18 +1,18 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appZoomIn]',
 })
 export class ZoomInDirective {
-  constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   @HostListener('mouseover', ['$event'])
-  zoomIn(event: MouseEvent) {
-    this.el.nativeElement.style.transform = 'scale(1.1)';
+  zoomIn() {
+    this.renderer.setStyle(this.el.nativeElement, 'transform', 'scale(1.1)');
   }
 
   @HostListener('mouseout', ['$event'])
-  zoomOut(event: MouseEvent) {
-    this.el.nativeElement.style.transform = 'scale(1)';
+  zoomOut() {
+    this.renderer.setStyle(this.el.nativeElement, 'transform', 'scale(1)');
   }
 }
