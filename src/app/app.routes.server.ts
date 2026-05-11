dc702d10acbf3fd6a7d609a6d41d20934c7457d7
DimitriTsikaridze <dtsikaridze@proton.me>
2026-05-12 00:40:44 +0400

    add server routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,12 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'champions/:id',
+    renderMode: RenderMode.Client,
+  },
+
+  {
     path: '**',
-    renderMode: RenderMode.Prerender,
+    renderMode: RenderMode.Server,
   },
 ];
