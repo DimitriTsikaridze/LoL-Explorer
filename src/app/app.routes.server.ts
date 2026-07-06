7cdd8dc7c0f8d6134ae5c832987c872b572bccdc
DimitriTsikaridze <dtsikaridze@proton.me>
2026-07-06 19:56:59 +0400

    change rendermode to server and add proxy headers

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,12 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'champions/:id',
-    renderMode: RenderMode.Client,
-  },
-
-  {
     path: '**',
-    renderMode: RenderMode.Prerender,
+    renderMode: RenderMode.Server,
   },
 ];
