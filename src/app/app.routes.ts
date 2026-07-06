84f8dbd58dfcec3ca3a613d0910e55240f39adcc
DimitriTsikaridze <dtsikaridze@proton.me>
2026-07-06 20:02:43 +0400

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,23 @@ export const routes: Routes = [
     loadChildren: () => import('./features/champions/champions.routes'),
     title: 'Champions',
   },
+
+  //   {
+  //   path: 'search-summoner',
+  //   loadChildren: () =>
+  //     import('./features/search-summoner/search-summoner.module').then(
+  //       (m) => m.SearchSummonerModule
+  //     ),
+  //   title: 'Search Summoner',
+  // },
+  //   {
+  //   path: 'champion-rotations',
+  //   loadChildren: () =>
+  //     import('./features/champion-rotations/champion-rotations.module').then(
+  //       (m) => m.ChampionRotationsModule
+  //     ),
+  //   title: 'Champion Rotationns',
+  // },
   {
     path: 'not-found',
     loadComponent: () => import('./features/error/error.component'),
