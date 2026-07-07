669f2165e69224eed4a5528dd1da371775c52206
DimitriTsikaridze <dtsikaridze@proton.me>
2026-07-07 17:55:51 +0400

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,4 +7,5 @@ export const environment = {
   championIconURL: `${dataDragonURL}img/champion/`,
   championDetailsURL: `${dataDragonURL}11.24.1/data/en_US/champion/`,
   apiKey: 'RGAPI-7224dab6-6171-4ad3-ba8e-5a63c28326f3',
+  ROTATIONS_URL: `https://eun1.api.riotgames.com/lol/platform/v3/champion-rotations/?api_key=RGAPI-7224dab6-6171-4ad3-ba8e-5a63c28326f3`, //?api_key=${API_KEY},
 };
